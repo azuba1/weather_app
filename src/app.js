2a0bafc0cb3aa58904668eaa2346ed5afd679a03
Joseph Azumah Bayata <azubayata@gmail.com>
2020-10-05 08:45:01 +0000

    checking for connection

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 // body-parser config
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -15,28 +15,29 @@ app.post("/", (req, res) => {
   const query = req.body.cityName;
   const apiKey = "877b8936e124398b97a294a4807b126b";
   const units = "metric";
-  
+
   //url for weather api
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${units}`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${units}`;
 
   https.get(url, (response) => {
-  response.on("data", (data) => {
-    const weatherData = JSON.parse(data);
-    const temp = weatherData.main.temp;
-    const weatherDescription = weatherData.weather[0].description;
-    const icon = weatherData.weather[0].icon;
-    const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-    res.write(`<p>The weather currently ${weatherDescription}</p>`);
-    res.write(`<h1>The temperature in ${query} is ${temp} degrees Celcius.</h1>`);
-    res.write(`<img src=${imageURL}>`);
-    res.send();
-  })
-  })
-})
+    response.on("data", (data) => {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+      res.write(`<p>The weather currently ${weatherDescription}</p>`);
+      res.write(
+        `<h1>The temperature in ${query} is ${temp} degrees Celcius.</h1>`
+      );
+      res.write(`<img src=${imageURL}>`);
+      res.send();
+    });
+  });
+});
 
 // Server Port
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}...`);
+  console.log(`Server is now running on port ${port}...`);
 });
-
